4ac00443b013b02c997b3dd39da70f814ef13337
James Hobin <hobinjk@mit.edu>
2015-12-12 00:21:08 -0500

    Refactor to have getPoints function

--- panorama.js
+++ panorama.js
@@ -28,19 +28,27 @@ function onVideoLoaded() {
   drawVideoFrame();
 }
 
+var debug = true;
+
+function getPoints(ctx) {
+  var imageData = ctx.getImageData(0, 0, width, height);
+  var harris = detectHarrisFeatures(imageData);
+  var points = maxSupress(harris);
+  if (debug) {
+    ctx.fillStyle = '#ffa400';
+    for (var i = 0; i < points.rows; i++) {
+      var x = points.data[i][1];
+      var y = points.data[i][0];
+      ctx.fillRect(x - 5, y - 5, 11, 11);
+    }
+  }
+}
+
 function drawVideoFrame() {
   window.requestAnimationFrame(drawVideoFrame);
   if (video.readyState === video.HAVE_ENOUGH_DATA) {
     videoImageContext.drawImage(video, 0, 0);
-    var imageData = videoImageContext.getImageData(0, 0, width, height);
-    var harris = detectHarrisFeatures(imageData);
-    var points = maxSupress(harris);
-    videoImageContext.fillStyle = '#ffa400';
-    for (var i = 0; i < points.rows; i++) {
-      var x = points.data[i][1];
-      var y = points.data[i][0];
-      videoImageContext.fillRect(x - 5, y - 5, 11, 11);
-    }
+    var points = getPoints(videoImageContext);
   }
 }
 
